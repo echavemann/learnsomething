5dcbbb2eb310325e113319cc0e9adaf57268466d
Ethan Havemann <echavemann@gmail.com>
2023-09-13 11:46:45 -0500

    cursed wip commit

---
+++ tst/test_bot.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(HackathonTests, BasicAssertion) {
+    EXPECT_EQ(4, 4);
+}
+
+TEST(HackathonTests, BasicAssertion2) {
+  EXPECT_EQ(5, 5);
+}
--- tst/test_trade.cpp
+++
@@ -1,9 +0,0 @@
-#include "trade.h"
-#include <gtest/gtest.h>
-
-TEST(TradeRiskTrackerTest, TradeInit) {
-    Trade testTrade(7, true, 1.2);
-    EXPECT_EQ(testTrade.quantity, 7);
-    EXPECT_NEAR(testTrade.price, 1.2, 1e-6); //fp
-    EXPECT_EQ(testTrade.side, true);
-}
