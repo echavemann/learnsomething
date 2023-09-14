4d2c3a77cc3545f1b6c089d12e32aee327ef3292
minnce <chrisminn205@gmail.com>
2023-09-14 13:14:43 -0500

    wtf

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,14 @@
 #include "pricingutil.h"
+
+PricingUtil::PricingUtil() {
+}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+    float res = (prevPrice * (0.9 + interest)) * oleoConstant;
+    val = res;
+    return res;
+}
+
+float PricingUtil::getVal() {
+    return val;
+}
