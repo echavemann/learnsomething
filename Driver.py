a2103570d1f6880f53a2e7ddb957d9e0886f6781
Ethan Havemann <echavemann@gmail.com>
2023-09-13 19:13:39 -0500

    Fixing

--- Driver.py
+++ Driver.py
@@ -2,5 +2,5 @@ import cppimport.import_hook
 import PricingUtil
 
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     print("Nice work on creating the util! Make sure to check the unit tests!")
--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,5 +1,5 @@
 //
 // Created by Ethan on 8/18/2023.
 //
-
+#include <pybind11/pybind11.h>
 #include "pricingutil.h"
