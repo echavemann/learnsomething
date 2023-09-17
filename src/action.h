dd31aa45fa98699197e3445b7b5086fe9af6365b
minnce <chrisminn205@gmail.com>
2023-09-17 17:45:45 -0500

    action h file

--- src/action.h
+++ src/action.h
@@ -1,18 +1,17 @@
-//
-// Created by Ethan on 9/13/2023.
-//
+    #ifndef LEARNSOMETHING_ACTION_H
+    #define LEARNSOMETHING_ACTION_H
 
-#ifndef LEARNSOMETHING_ACTION_H
-#define LEARNSOMETHING_ACTION_H
+    class Action {
+    public:
+        Action();
+        double initialBalance;
+        bool buy(double price);
+        bool sell(double price);
+        bool getHold();
+        double getBalance();
+    private:
+        double balance;
+        bool holding;   
+    };
 
-
-class Action {
-public:
-    bool buy;
-    Action(bool buyInput);
-    bool getAction();
-    void setAction(bool buyInput);
-};
-
-
-#endif //LEARNSOMETHING_ACTION_H
+    #endif //LEARNSOMETHING_ACTION_H
