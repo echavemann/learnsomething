12339b6d78822906c9c1f49437dfaaffd416010d
minnce <chrisminn205@gmail.com>
2023-09-17 17:56:15 -0500

    init tests for action

--- tst/test_bot.cpp
+++ tst/test_bot.cpp
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
+#include "action.h"
 
 TEST(HackathonTests, BasicAssertion) {
-    EXPECT_EQ(4, 4);
+    EXPECT_NEAR(4, 4, 1e-4);
 }
 
 TEST(HackathonTests, BasicAssertion2) {
-  EXPECT_EQ(5, 5);
+  EXPECT_NEAR(5, 5, 1e-4);
+}
+
+TEST(ActionTest, PleaseWork) {
+  Action action;
+  float a = action.getBalance();
+  bool b = action.buy(500);
+  EXPECT_EQ(b,false);
+  EXPECT_NEAR(a,0,1e-4);
 }
