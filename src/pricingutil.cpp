3548ed83cb4efdc52f4e3115d4ece83fa216dbca
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-09-21 19:11:20 -0500

    calc and get value functions in C++ with corresponding unit tests

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,13 @@
 #include "pricingutil.h"
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {
+  float result = (prevPrice * (0.9 + interest)) * oleoConstant;
+  this->val = result;
+  return result;
+}
+
+float PricingUtil::getVal() {
+  return this->val;
+}
+
+PricingUtil::PricingUtil() {}
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,14 @@
 #include <gtest/gtest.h>
 
-TEST(sampleTest, sample) {
-    EXPECT_EQ(4, 4);
+#include "pricingutil.h"
+
+TEST(PricingUtilFormula, TestFormula) {
+    PricingUtil util = PricingUtil();
+
+    EXPECT_NEAR(util.calcVal(1, 0.1, 3), 3, 1e-4);
+    EXPECT_NEAR(util.getVal(), 3, 1e-4);
+    EXPECT_NEAR(util.calcVal(30, 0.1, 3), 90, 1e-4);
+    EXPECT_NEAR(util.getVal(), 90, 1e-4);
+    EXPECT_NEAR(util.calcVal(30, 0.5, 3), 126, 1e-4);
+    EXPECT_NEAR(util.getVal(), 126, 1e-4);
 }
