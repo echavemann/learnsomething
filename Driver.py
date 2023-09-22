0fbc96e05af4a180bfc4a7852a2e03f3713ca981
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-09-21 19:10:55 -0500

    Python code with corresponding unit tests

--- Driver.py
+++ Driver.py
@@ -1,6 +1,15 @@
 import cppimport.import_hook
 import PricingUtil
+import unittest
 
+class TestGetAreaRectangle(unittest.TestCase):
+    def runTest(self):
+        util = PricingUtil.PricingUtil()
+        self.assertAlmostEqual(util.calcVal(1, 0.1, 3), 3, 3, "doesn't match")
+        self.assertAlmostEqual(util.getVal(), 3, 3, "doesn't match")
+        self.assertAlmostEqual(util.calcVal(30, 0.1, 3), 90, 3, "doesn't match")
+        self.assertAlmostEqual(util.getVal(), 90, 3, "doesn't match")
 
 if __name__ == "__main__":
     print("Nice work on creating the util! Make sure to check the unit tests!")
+    unittest.main()
