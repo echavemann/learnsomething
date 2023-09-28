4740dc96415177df7291fbd9b5b75eeb4aadda57
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2023-09-27 20:45:59 -0500

    Create exchange interface class and add unit tests

--- src/interface.py
+++ src/interface.py
@@ -1,13 +1,29 @@
+from collections import deque
+
 class Exchange:
     # implement this!
 
-    def __init__(self, initialBalance):
+    def __init__(self, initialBalance, validTrades):
         """Initial Balance is the amount that each account should start with."""
-        pass
+        self.pending = {}
+        self.initialBalance = initialBalance
+        self.valid_stocks = set(validTrades)
 
     def add_trade(self, trade):
         """Adds a trade to the exchange (validation required)
         and returns a match if required. It is up to you on how you will
         handle representing trades. """
-        raise NotImplementedError
-
+        try:
+            (stock, requester) = trade
+        except:
+            raise Exception
+        
+        if stock not in self.valid_stocks:
+            raise Exception
+        
+        if not stock in self.pending or not self.pending[stock]:
+            self.pending[stock] = deque([requester])
+            return None
+        else:
+            counterparty = self.pending[stock].popleft()
+            return (stock, requester, counterparty)
