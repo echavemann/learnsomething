fe17a7c9ff64f777c369723a33305a3656948ca8
Lam-Richard <richardlam2024@gmail.com>
2023-10-04 20:13:51 -0500

    finished Step 1

--- src/pricingutil.cpp
+++ src/pricingutil.cpp
@@ -1,1 +1,15 @@
-#include "pricingutil.h"
+#include "PricingUtil.h"
+
+PricingUtil::PricingUtil() {
+    val = 0.0;
+}
+
+float PricingUtil::calcVal(float prevPrice, float interest, float oleoConstant) {  
+    float newVal = (prevPrice * (0.9 + interest)) * oleoConstant;
+    this->val = newVal;
+    return this->val;
+}
+
+float PricingUtil::getVal() {
+    return this->val;
+}
--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,36 @@
 #include <gtest/gtest.h>
+#include <pricingutil.h>
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
+}
+
+TEST(PricingUtilTest, simpleTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.calcVal(1.0, 1.0, 1.0), 1.9);
+}
+
+TEST(PricingUtilTest, zeroTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.calcVal(0.0, 0.0, 0.0), 0.0);
+}
+
+TEST(PricingUtilTest, negativePrevPriceTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.calcVal(-1.0, 0.1, 1.0), -1.0);
+}
+
+TEST(PricingUtilTest, negativeOleoConstantTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.calcVal(2.0, 1.1, -1.0), -4.0);
+}
+
+TEST(PricingUtilTest, negativeInterestRateTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.calcVal(2.0, -0.4, 1.0), 1.0);
+}
+
+TEST(PricingUtilTest, getValTest) {
+    PricingUtil testPricingUtil;
+    EXPECT_EQ(testPricingUtil.getVal(), 0);
 }
